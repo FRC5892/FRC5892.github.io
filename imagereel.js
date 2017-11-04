4af436af102b31ab5096664735054094de23f5e3
quantumAssembly <kaibug@gmail.com>
2017-11-04 10:35:18 -0500

    Improved loading for image reel

--- imagereel.js
+++ imagereel.js
@@ -1,8 +1,24 @@
 const NUM_IMAGES = 5;
-let curImg = -1;
+let _curImg = -1;
 
-function advanceReel() {
-	curImg = (curImg + 1) % NUM_IMAGES;
-	document.getElementById("imagereel").style.backgroundImage = "url(images/image-reel/" + curImg + ".png)";
-	setTimeout(advanceReel, 4000);
+function initImageReel() {
+	_loadImage(0);
+	_advanceReel();
+}
+
+// http://blog.teamtreehouse.com/learn-asynchronous-image-loading-javascript
+function _loadImage(num) {
+	if (!(0 <= num && num < NUM_IMAGES)) return;
+	let dlImage = new Image();
+	dlImage.onload = function() {
+		document.getElementById("img-load-area").innerHTML += "<img src=\"" + this.src + "\" />"
+		_loadImage(num+1);
+	};
+	dlImage.src = "images/image-reel/" + num + ".png";
+}
+
+function _advanceReel() {
+	_curImg = (_curImg + 1) % NUM_IMAGES;
+	document.getElementById("imagereel").style.backgroundImage = "url(images/image-reel/" + _curImg + ".png)";
+	setTimeout(_advanceReel, 4000);
 }
